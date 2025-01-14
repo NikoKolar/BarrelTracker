3d188c2d70d42b47879f55d35414c8d412a341d8
Niko <niko.kolar@gmail.com>
2025-01-14 19:04:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BarrelTracker',
+  webDir: 'www'
+};
+
+export default config;
